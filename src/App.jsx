216def8f4f2adc532e2adf2925866b607ccaf007
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-05-18 16:52:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Components/Header"
 import Body from "./Components/Body"
 import Footer from "./Components/Footer"
-import "./index.css"
+import "./index.css";
 
 function App() {
   
